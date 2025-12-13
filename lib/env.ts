0fabd0e8310d82ab74274c6cc68801e82181cf77
Dhanya MD <dhanyamd@Dhanyas-MacBook-Air.local>
2025-12-13 21:26:27 +0530

    wrap up authentication

--- lib/env.ts
+++ lib/env.ts
@@ -1,16 +1,17 @@
+import "dotenv/config";
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
- 
-export const env = createEnv({
-  server: {
-    GITHUB_CLIENT_ID: z.string().min(1),
-    GITHUB_CLIENT_SECRET: z.string().min(1),  
-    GOOGLE_CLIENT_ID: z.string().min(1),
-    GOOGLE_CLIENT_SECRET: z.string().min(1),
-    GOOGLE_GENERATIVE_AI_API_KEY: z.string().min(1),
-  
-  },
 
- 
-  experimental__runtimeEnv: process.env
+export const env = createEnv({
+    server: {
+        GITHUB_CLIENT_ID: z.string().min(1),
+        GITHUB_CLIENT_SECRET: z.string().min(1),
+        GOOGLE_CLIENT_ID: z.string().min(1),
+        GOOGLE_CLIENT_SECRET: z.string().min(1),
+        GOOGLE_GENERATIVE_AI_API_KEY: z.string().min(1),
+
+    },
+
+
+    experimental__runtimeEnv: process.env
 });
